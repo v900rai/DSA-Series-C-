7cef85a95365ce6b23c35c6c147239626aa3e62b
vishal rai <vishalrai10797@gmail.com>
2024-09-10 19:35:48 +0530

    ecdfjfkgwdygy

---
+++ PROGRAM/age.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int age;
+  cout << "Enter the age : ";
+  cin >> age;
+  if (age >= 18)
+  {
+    cout << "ypu can vote \n";
+  }
+  else
+  {
+    cout << " ypu can't vote \n";
+  }
+  return 0;
+}
