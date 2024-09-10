ea8c9d7c058825cbe12dfaba7cf141bf07476c64
vishal rai <vishalrai10797@gmail.com>
2024-09-10 19:24:57 +0530

    looping statement

---
+++ PROGRAM/loop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int n = 99;
+  if (n >= 0)
+  {
+    cout << "n postive number \n";
+  }
+  else
+  {
+    cout << "n is negative number\n";
+  }
+  return 0;
+
+} // namespace std;
